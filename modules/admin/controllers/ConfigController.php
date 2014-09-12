779cca020743606d9bc7d7150eed80739c048d0e
疯狂的dabing <xbzbing@gmail.com>
2014-09-12 23:58:27 +0800

    后台config设计

--- modules/admin/controllers/ConfigController.php
+++ modules/admin/controllers/ConfigController.php
@@ -12,47 +12,32 @@ use app\modules\admin\components\Controller;
 class ConfigController extends Controller
 {
 
-	/**
-	 * @inheritdoc
-	 */
-	public function actions()
-	{
-		return [
-			'error' => [
-				'class' => 'yii\web\ErrorAction',
-			],
-		];
+
+    /**
+     * Base Setting
+     * 基本设置
+     */
+    public function actionSetting(){
 	}
 
-	public function actionIndex()
-	{
-        $this->layout = 'column2';
-        $server['serverSoft'] = $_SERVER['SERVER_SOFTWARE'];
-        $server['serverOs'] = PHP_OS;
-        $server['phpVersion'] = PHP_VERSION;
-        $server['fileupload'] = ini_get('file_uploads') ? ini_get('upload_max_filesize') : '禁止上传';
-        $server['serverUri'] = $_SERVER['SERVER_NAME'];
-        $server['maxExcuteTime'] = ini_get('max_execution_time') . ' 秒';
-        $server['maxExcuteMemory'] = ini_get('memory_limit');
-        $server['magic_quote_gpc'] = get_magic_quotes_gpc() ? '开启' : '关闭';
-        $server['allow_url_fopen'] = ini_get('allow_url_fopen') ? '开启' : '关闭';
-        $server['excuteUseMemory'] = function_exists('memory_get_usage') ? XUtils::dataFormat(memory_get_usage()) : '未知';
-        $dbsize = 0;
-        $connection = Yii::$app->db;
-        $sql = "SHOW TABLE STATUS LIKE '{$connection->tablePrefix}%'";
-        $command = $connection->createCommand($sql)->queryAll();
-        foreach ($command as $table)
-            $dbsize += $table['Data_length'] + $table['Index_length'];
-        $mysqlVersion = $connection->createCommand("SELECT version() AS version")->queryAll();
-        $server['mysqlVersion'] = $mysqlVersion[0]['version'];
-        $server['dbsize'] = $dbsize ? XUtils::dataFormat($dbsize) : '未知';
-		return $this->render('index',['server'=>$server]);
+    /**
+     * 系统SEO设置
+     */
+    public function actionSEO(){
 	}
 
-	public function actionLocale($language)
-	{
-		Common::setLanguage($language);
-		return $this->redirect(['index']);
-	}
+    /**
+     * 系统Slider设置
+     */
+    public function actionSlider(){
+
+    }
+
+    /**
+     * 系统缓存设置
+     */
+    public function actionCache(){
+
+    }
 
 }
