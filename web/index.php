0b84075bfd9c56c2922f9139b005e3f0a11178b1
疯狂的dabing <xbzbing@gmail.com>
2016-08-22 11:04:12 +0800

    [#] 去掉开发模式特征

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', false);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../vendor/autoload.php');
